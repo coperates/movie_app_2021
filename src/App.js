1d128fbf7160fbd73a1bcf7343d5610a777e8100
coperates <dlwoals1195@gmail.com>
2021-09-03 15:58:44 +0900

    My first React App Update3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import Potato from "./Potato";
+
+//props를 받아서 출력 porps.fav = {fav}는 같은 뜻임
+function Food({fav}){
+  return(
+    <h1>I like {fav}</h1>
+  );
+}
 
 function App() {
   return (
-    <div><h1>Good morning</h1><Potato /></div>
+    <div>
+      <h1>Good morning</h1>
+      <Food fav="kimchi"/>
+      <Food fav="remen"/>
+      <Food fav="cukumi"/>
+      <Food fav="potato"/>
+
+    </div>
   );
 }
 
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Potato(){
-    return(
-        <h1>test</h1>
-    );
-}
-
-export default Potato;
